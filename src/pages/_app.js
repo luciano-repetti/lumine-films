0731d6adb30c76d435326724f74c69bbce683e89
Luciano Repetti <lucianorepetti2020@gmail.com>
2023-02-17 11:45:11 -0300

    page home base

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Header from 'components/Header'
+import '../styles/style.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
